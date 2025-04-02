65e0322c0f1098f671e56850ef0dc1c286f08437
ProjetoAndroid <jgfss_2014@hotmail.com>
2025-04-02 18:17:52 -0300

    Adicionando alteracoes 02/04

--- js/carousel.js
+++ js/carousel.js
@@ -1,50 +1,54 @@
-
-
-//carousel
-
-//Array storage class
 let carouselArr = [];
 
-
-//class Carousel
 class Carousel {
-    constructor (img, descricao, link){
+    constructor(img, descricao, link) {
         this.img = img;
         this.descricao = descricao;
         this.link = link;
     }
-    
-      
-    static Start(arr){
-        if(arr){
 
-            if(arr.length > 0){
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
+    static Start(arr) {
+        if (arr) {
+            if (arr.length > 0) {
+                Carousel._sequence = 0;  
+                Carousel._size = arr.length; 
+                Carousel.Next();  
+                Carousel._interval = setInterval(function () { Carousel.Next(); }, 2000); 
             }
-            
         } else {
-            throw "Method Start need a Array Variable.";
+            throw "Method Start needs an Array Variable."; 
         }
     }
 
-    static Next(){
+    static Next() {
         const img = document.getElementById('img');
         const descricao = document.getElementById('descricao');
         const link = document.getElementById('link');
 
-        
-      
         img.src = carouselArr[Carousel._sequence].img;
         descricao.innerText = carouselArr[Carousel._sequence].descricao;
         link.href = carouselArr[Carousel._sequence].link;
-        
+
         Carousel._sequence++;
-        
-        if(Carousel._sequence >= Carousel._size){
+
+        if (Carousel._sequence >= Carousel._size) {
             Carousel._sequence = 0;
         }
     }
-};
+
+    static Prev() {
+        const img = document.getElementById('img');
+        const descricao = document.getElementById('descricao');
+        const link = document.getElementById('link');
+
+        img.src = carouselArr[Carousel._sequence].img;
+        descricao.innerText = carouselArr[Carousel._sequence].descricao;
+        link.href = carouselArr[Carousel._sequence].link;
+
+        Carousel._sequence--;
+
+        if (Carousel._sequence < 0) {
+            Carousel._sequence = Carousel._size - 1;
+        }
+    }
+}
